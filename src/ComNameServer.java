6aa1c16b3a29587fa136f5e21fa4692fdaeb8050
Nicholas <n1_biker@hotmail.com>
2015-03-16 00:51:51 -0400

    added simple classes

---
+++ src/ComNameServer.java
@@ -0,0 +1,10 @@
+
+public class ComNameServer implements NameServerInterface{
+
+	@Override
+	public String getAddress(String address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
